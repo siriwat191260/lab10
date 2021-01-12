74dbdbc75a584748081112ca9afdc5f203138778
siriwat191260 <pan-191260@hotmail.com>
2021-01-12 16:40:32 +0700

    finish

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,2 +1,21 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+int main(){
+    string grade[9] = {"A","B+","B","C+","C","D+","D","F","W"};
+    srand(time(0));
+    int i = rand() % 9 + 0;
+    int p = 0;
+    string x;
+    cout << "Press Enter 3 times to reveal your future.";
+    while(p<3){
+    	cin.get();
+    	p++;
+	}
+   
+   
+    cout << "You will get " << grade[i] << " in this 261102.";
+    return 0;
+}
